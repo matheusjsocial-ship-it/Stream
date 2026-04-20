561bffcfdbb1b082c13c9d4464826daab3f59fd0
matheusjsocial-ship-it <matheusjsocial@gmail.com>
2026-04-20 19:27:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './linguastream';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
